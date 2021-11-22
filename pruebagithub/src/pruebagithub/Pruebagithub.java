8682c132861e14062ed3e1585ef6bcbd44d68004
evaar99 <94857563+evaar99@users.noreply.github.com>
2021-11-22 17:46:43 +0100

    Add files via upload

---
+++ pruebagithub/src/pruebagithub/Pruebagithub.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebagithub;
+
+/**
+ *
+ * @author evaren
+ */
+public class Pruebagithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Esto es una prueba para subir al repositorio de github. Hola, soy Eva.");
+    }
+    
+}
